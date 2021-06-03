4d54bc056f21e346e15b5e79cc62761a99f1a89f
Mohamed El omary <mel-omar@e2r8p5.1337.ma>
2021-06-03 16:51:11 +0100

    day00 retry

--- module00/ex01/srcs/main.cpp
+++ module00/ex01/srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mel-omar <mel-omar@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: mel-omar <mel-omar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 13:29:47 by mel-omar          #+#    #+#             */
-/*   Updated: 2021/06/02 23:23:22 by mel-omar         ###   ########.fr       */
+/*   Updated: 2021/06/03 13:30:20 by mel-omar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,19 +43,23 @@ int     main(void)
             break ;
         else if (command == "ADD")
         {
+
             Contact con;
-            con.set_first(fill_field("first name"));
-            con.set_last(fill_field("last name"));
-            con.set_nickname(fill_field("nickname"));
-            con.set_login(fill_field("login"));
-            con.set_eaddress(fill_field("email address"));
-            con.set_paddress(fill_field("postal address"));
-            con.set_phonenumber(fill_field("phone number"));
-            con.set_birthaday(fill_field("Birthday"));
-            con.set_favorite_meal(fill_field("favorite meal"));
-            con.set_underwear_color(fill_field("underwear color"));
-            con.set_darkest_secret(fill_field("darkest secret"));
-            con.set_index(phone.get_total() + 1);
+            if (phone.get_total() < 8)
+            {
+                con.set_first(fill_field("first name"));
+                con.set_last(fill_field("last name"));
+                con.set_nickname(fill_field("nickname"));
+                con.set_login(fill_field("login"));
+                con.set_eaddress(fill_field("email address"));
+                con.set_paddress(fill_field("postal address"));
+                con.set_phonenumber(fill_field("phone number"));
+                con.set_birthaday(fill_field("Birthday"));
+                con.set_favorite_meal(fill_field("favorite meal"));
+                con.set_underwear_color(fill_field("underwear color"));
+                con.set_darkest_secret(fill_field("darkest secret"));
+                con.set_index(phone.get_total() + 1);
+            }
             phone.add_contact(con);
         }
         else if (command == "SEARCH")
