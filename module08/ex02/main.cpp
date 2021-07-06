031ef691633e233173ccfdab96024ce514672621
Mohamed El omary <mel-omar@s1r5p2.1337.ma>
2021-07-06 12:57:57 +0100

    day08 finish

---
+++ module08/ex02/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-omar <mel-omar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/06 11:29:24 by mel-omar          #+#    #+#             */
+/*   Updated: 2021/07/06 12:01:59 by mel-omar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mutantstack.hpp"
+#include <iostream>
+
+
+int main()
+{
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    return (0);
+}
---
+++ module08/ex02/mutantstack.hpp
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-omar <mel-omar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/06 11:29:34 by mel-omar          #+#    #+#             */
+/*   Updated: 2021/07/06 12:26:30 by mel-omar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        MutantStack();
+        MutantStack(const MutantStack &stack);
+        MutantStack & operator = (const MutantStack &stack);
+        ~MutantStack(void);
+        iterator begin(void);
+        iterator end(void);
+        reverse_iterator    rbegin(void);
+        reverse_iterator    rend();
+        
+};
+
+template <typename T>
+MutantStack<T>::MutantStack() : std::stack<T>()
+{
+
+}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack<T> & mstack) : std::stack<T>(mstack)
+{
+
+}
+
+template <typename T>
+MutantStack<T> & MutantStack<T>::operator = (const MutantStack<T> & mstack)
+{
+    std::stack<T>::operator = (mstack);
+    return (*this);
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack()
+{
+
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin(void)
+{
+    return std::stack<T>::c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end(void)
+{
+    return std::stack<T>::c.end();
+}
+
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rbegin(void)
+{
+    return std::stack<T>::c.rbegin();
+}
+
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rend(void)
+{
+    return std::stack<T>::c.rend();
+}
+
+#endif
